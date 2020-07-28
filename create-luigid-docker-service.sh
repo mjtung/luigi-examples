3dcbe17fe057fb1708eaa84075b0ff3fc1f277e5
MJ Tung <tung.mj@mgmail.com>
2020-07-28 17:45:07 +0800

    MJ: make it work in swarm mode

--- create-luigid-docker-service.sh
+++ create-luigid-docker-service.sh
@@ -5,10 +5,10 @@
     # pass in custom run script
     # get image: TODO: can keep a copy of it somewhere
     # pass in logdir args to luigid command
-docker service create -d -p 8082:8082 --name docker-luigid-service --replicas 2 \
+docker service create -d -p 8082:8082 --name docker-luigid-service \
     --mount type=volume,source=luigistate,destination=/luigi/state \
-    --mount type=volume,source=/mnt/c/Users/tungm/Projects/luigi-examples/luigi.cfg,destination=/etc/luigi/luigi.cfg \
-    --mount type=volume,source=/mnt/c/Users/tungm/Projects/luigi-examples/luigid-start.sh,destination=/bin/run \
-    --mount type=volume,source=/mnt/c/Users/tungm/Projects/luigi-server/log,destination=/var/log/luigi \
+    --mount type=bind,source=/mnt/c/Users/tungm/Projects/luigi-examples/luigi.cfg,destination=/etc/luigi/luigi.cfg \
+    --mount type=bind,source=/mnt/c/Users/tungm/Projects/luigi-examples/luigid-start.sh,destination=/bin/run \
+    --mount type=bind,source=/mnt/c/Users/tungm/Projects/luigi-server/log,destination=/var/log/luigi \
     axiom/docker-luigi:2.8.13-alpine \
     --logdir /var/log/luigi  
